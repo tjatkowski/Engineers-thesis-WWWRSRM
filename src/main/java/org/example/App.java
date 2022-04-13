271405fd3c023bca45c46322ee89578475c07f69
nwolniak <niedzwiedz107@gmail.com>
2022-04-14 00:05:19 +0200

    Initialize Project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+}
