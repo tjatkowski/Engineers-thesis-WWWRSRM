c241169e1b4a2a0f5701d0f1aaaeec26162d250e
nwolniak <niedzwiedz107@gmail.com>
2022-05-31 15:42:46 +0200

    receiving messages, spring boot, kafka and jafafx together

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,28 @@
 module SimulationVisualization {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires lombok;
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    requires spring.kafka;
+    requires javafx.controls;
+    requires javafx.fxml;
     requires osmosis.pbf;
     requires osmosis.core;
+    requires lombok;
+    requires org.slf4j;
+    requires kafka.clients;
+    requires java.sql;
 
 
     opens pl.edu.agh.wwwrsrm.visualization to javafx.fxml;
+    opens pl.edu.agh.wwwrsrm;
+    opens pl.edu.agh.wwwrsrm.consumer.config to spring.core;
+    opens pl.edu.agh.wwwrsrm.consumer to spring.core;
+    exports pl.edu.agh.wwwrsrm.consumer to spring.beans;
+    exports pl.edu.agh.wwwrsrm.consumer.config to spring.beans, spring.context;
     exports pl.edu.agh.wwwrsrm.visualization;
     exports pl.edu.agh.wwwrsrm.graph;
     exports pl.edu.agh.wwwrsrm.osm;
+    exports pl.edu.agh.wwwrsrm;
 }
--- src/main/java/pl/edu/agh/wwwrsrm/utils/CoordinatesConverter.java
+++ src/main/java/pl/edu/agh/wwwrsrm/utils/CoordinatesConverter.java
@@ -2,22 +2,22 @@ package pl.edu.agh.wwwrsrm.utils;
 
 import javafx.geometry.Point2D;
 
-public class CoordinatesConverter {
+public interface CoordinatesConverter {
 
-    public static double convertLongitudeToX(double longitude, int zoomLevel) {
+    static double convertLongitudeToX(double longitude, int zoomLevel) {
         return ((longitude + 180) / 360) * 256 * Math.pow(2, zoomLevel);
     }
 
-    public static double convertLatitudeToY(double latitude, int zoomLevel) {
+    static double convertLatitudeToY(double latitude, int zoomLevel) {
         double sinLatitude = Math.sin(latitude * Math.PI / 180);
         return (0.5 - Math.log((1 + sinLatitude) / (1 - sinLatitude)) / (4 * Math.PI)) * 256 * Math.pow(2, zoomLevel);
     }
 
-    public static double convertXToLongitude(double x, int zoomLevel) {
+    static double convertXToLongitude(double x, int zoomLevel) {
         return ((x / (256 * Math.pow(2, zoomLevel))) - 0.5) * 360;
     }
 
-    public static double convertYToLatitude(double y, int zoomLevel) {
+    static double convertYToLatitude(double y, int zoomLevel) {
         return 90 - 360 * Math.atan(Math.exp(-(0.5 - (y / (256 * Math.pow(2, zoomLevel)))) * 2 * Math.PI)) / Math.PI;
     }
 
@@ -27,7 +27,7 @@ public class CoordinatesConverter {
      * @param x global coordinate
      * @return x view local coordinate
      */
-    public static double scaleXToFitWindow(double x, Point2D minBound, Point2D maxBound, int mapWidth, int zoomLevel) {
+    static double scaleXToFitWindow(double x, Point2D minBound, Point2D maxBound, int mapWidth, int zoomLevel) {
         double minBoundX = convertLongitudeToX(minBound.getX(), zoomLevel);
         double maxBoundX = convertLongitudeToX(maxBound.getX(), zoomLevel);
         return ((x - minBoundX) / (maxBoundX - minBoundX)) * mapWidth;
@@ -39,7 +39,7 @@ public class CoordinatesConverter {
      * @param y global coordinate
      * @return y view local coordinate
      */
-    public static double scaleYToFitWindow(double y, Point2D minBound, Point2D maxBound, int mapHeight, int zoomLevel) {
+    static double scaleYToFitWindow(double y, Point2D minBound, Point2D maxBound, int mapHeight, int zoomLevel) {
         double minBoundY = convertLatitudeToY(minBound.getY(), zoomLevel);
         double maxBoundY = convertLatitudeToY(maxBound.getY(), zoomLevel);
         return ((y - minBoundY) / (maxBoundY - minBoundY)) * mapHeight;
