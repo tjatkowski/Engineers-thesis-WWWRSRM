dca4ad23d014208b7eb6236174abfd958da1c4f9
Knayder <knayder@Tomaszs-MacBook-Pro.local>
2022-08-21 19:13:05 +0200

    Rebase to new main

---
+++ src/main/java/pl/edu/agh/wwwrsrm/window/menu/Menu.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.wwwrsrm.window.menu;
+
+import javafx.scene.layout.VBox;
+
+public class Menu extends VBox {
+    public Menu(double width) {
+        setPrefWidth(width);
+        this.setStyle("-fx-background-color: #FF00FF;");
+    }
+}
