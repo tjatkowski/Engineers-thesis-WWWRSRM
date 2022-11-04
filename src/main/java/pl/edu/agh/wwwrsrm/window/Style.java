89f13b9a6459a10e1c533ae82a31d187df7958ca
nwolniak <niedzwiedz107@gmail.com>
2022-11-04 12:10:23 +0100

    Visualization stopping and resuming

--- src/main/java/pl/edu/agh/wwwrsrm/window/Style.java
+++ src/main/java/pl/edu/agh/wwwrsrm/window/Style.java
@@ -4,7 +4,7 @@ public class Style {
     public static double windowWidth = 1280;
     public static double windowHeight = 720;
 
-    public static double menuWidth = 50;
+    public static double menuWidth = 75;
 
     public static double maxFps = 25;
 }
--- src/main/java/pl/edu/agh/wwwrsrm/window/menu/Menu.java
+++ src/main/java/pl/edu/agh/wwwrsrm/window/menu/Menu.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Component;
 import pl.edu.agh.wwwrsrm.connection.producer.VisualizationStateChangeProducer;
 import pl.edu.agh.wwwrsrm.window.Style;
 
-import static proto.model.RUNNING_STATE.STARTED;
-import static proto.model.RUNNING_STATE.STOPPED;
+import static proto.model.RUNNING_STATE.*;
 
 
 @Component
@@ -22,12 +21,22 @@ public class Menu extends VBox {
 
 
         Button startButton = new Button("Start");
+        Button resumeButton = new Button("Resume");
         Button stopButton = new Button("Stop");
+        startButton.setPrefWidth(Style.menuWidth);
+        resumeButton.setPrefWidth(Style.menuWidth);
+        stopButton.setPrefWidth(Style.menuWidth);
         this.getChildren().add(startButton);
+        this.getChildren().add(resumeButton);
         this.getChildren().add(stopButton);
         startButton.setOnMouseClicked(event -> {
             if (MouseButton.PRIMARY.equals(event.getButton())) {
                 stateChanger.sendStateChangeMessage(STARTED);
+            }
+        });
+        resumeButton.setOnMouseClicked(event -> {
+            if (MouseButton.PRIMARY.equals(event.getButton())) {
+                stateChanger.sendStateChangeMessage(RESUMED);
             }
         });
         stopButton.setOnMouseClicked(event -> {
