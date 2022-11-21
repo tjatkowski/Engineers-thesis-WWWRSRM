4b9ea39d480856b93c5edeed3856c453bbefb078
Tomasz Jatkowski <tomaszjatkowski42@gmail.com>
2022-11-21 19:54:36 +0100

    Add polling of custom nodes

--- src/main/java/pl/edu/agh/wwwrsrm/graph/NodeOSM.java
+++ src/main/java/pl/edu/agh/wwwrsrm/graph/NodeOSM.java
@@ -2,6 +2,8 @@ package pl.edu.agh.wwwrsrm.graph;
 
 import lombok.Getter;
 import pl.edu.agh.wwwrsrm.utils.coordinates.LonLatCoordinate;
+import proto.model.Coordinates;
+import proto.model.Node;
 
 /**
  * NodeOSM implements road graph node
@@ -14,5 +16,11 @@ public class NodeOSM {
     public NodeOSM(long id, LonLatCoordinate coordinate) {
         this.id = id;
         this.coordinate = coordinate;
+    }
+
+    public NodeOSM(Node node) {
+        this.id = Long.parseLong(node.getNodeId());
+        Coordinates coords = node.getCoordinates();
+        this.coordinate = new LonLatCoordinate(coords.getLongitude(), coords.getLatitude());
     }
 }
