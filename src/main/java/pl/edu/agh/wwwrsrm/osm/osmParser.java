4e018c5cbef06094a0a151e9241f397ec059c30a
nwolniak <niedzwiedz107@gmail.com>
2022-11-24 21:53:54 +0100

    New json file with map parameters created

--- src/main/java/pl/edu/agh/wwwrsrm/osm/osmParser.java
+++ src/main/java/pl/edu/agh/wwwrsrm/osm/osmParser.java
@@ -1,19 +1,23 @@
 package pl.edu.agh.wwwrsrm.osm;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import crosby.binary.osmosis.OsmosisReader;
-import pl.edu.agh.wwwrsrm.graph.EdgeOSM;
-import pl.edu.agh.wwwrsrm.graph.GraphOSM;
-import pl.edu.agh.wwwrsrm.graph.NodeOSM;
-import pl.edu.agh.wwwrsrm.graph.WayOSM;
-import javafx.scene.paint.Color;
 import org.openstreetmap.osmosis.core.domain.v0_6.Node;
 import org.openstreetmap.osmosis.core.domain.v0_6.Tag;
 import org.openstreetmap.osmosis.core.domain.v0_6.Way;
 import org.openstreetmap.osmosis.core.domain.v0_6.WayNode;
+import pl.edu.agh.wwwrsrm.graph.EdgeOSM;
+import pl.edu.agh.wwwrsrm.graph.GraphOSM;
+import pl.edu.agh.wwwrsrm.graph.NodeOSM;
+import pl.edu.agh.wwwrsrm.graph.WayOSM;
 import pl.edu.agh.wwwrsrm.utils.coordinates.LonLatCoordinate;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 /**
@@ -21,53 +25,6 @@ import java.util.Map;
  * Only ways with 'highway' key parameter and values indicating possibility of car driving on them are considered.
  */
 public class osmParser {
-    public static final Map<String, WayParameters> roadZooms = Map.ofEntries(
-            // roads
-            Map.entry("motorway", new WayParameters(5, 30, Color.WHITE, "highway")),
-            Map.entry("trunk", new WayParameters(9, 8, Color.WHITE, "highway")),
-            Map.entry("primary", new WayParameters(10, 20, Color.WHITE, "highway")),
-            Map.entry("secondary", new WayParameters(11, 17, Color.WHITE, "highway")),
-            Map.entry("tertiary", new WayParameters(12, 13, Color.WHITE, "highway")),
-            Map.entry("unclassified", new WayParameters(14, 8, Color.WHITE, "highway")),
-            Map.entry("residential", new WayParameters(15, 8, Color.WHITE, "highway")),
-            // link roads
-            Map.entry("motorway_link", new WayParameters(11, 30, Color.WHITE, "highway")),
-            Map.entry("trunk_link", new WayParameters(12, 8, Color.WHITE, "highway")),
-            Map.entry("primary_link", new WayParameters(13, 20, Color.WHITE, "highway")),
-            Map.entry("secondary_link", new WayParameters(14, 17, Color.WHITE, "highway")),
-            Map.entry("tertiary_link", new WayParameters(15, 13, Color.WHITE, "highway")),
-            // special roads
-//            Map.entry("living_street", new WayParameters(17, 10, Color.PAPAYAWHIP, "highway")),
-//            Map.entry("service", new WayParameters(16, 10, Color.PAPAYAWHIP, "highway")),
-//            Map.entry("pedestrian", new WayParameters(16, 10, Color.PAPAYAWHIP, "highway")),
-//            Map.entry("track", new WayParameters(15, 10, Color.PAPAYAWHIP, "highway")),
-//            Map.entry("bus_guideway", new WayParameters(16, 10, Color.PAPAYAWHIP, "highway")),
-//            Map.entry("escape", new WayParameters(16, 10, Color.PAPAYAWHIP, "highway")),
-//            Map.entry("raceway", new WayParameters(16, 10, Color.PAPAYAWHIP, "highway")),
-//            Map.entry("road", new WayParameters(16, 10, Color.PAPAYAWHIP, "highway")),
-//            Map.entry("busway", new WayParameters(13, 10, Color.PAPAYAWHIP, "highway")),
-//            // waterway
-            Map.entry("river", new WayParameters(10, 60, Color.BLUE, "waterway")),
-//            Map.entry("stream", new WayParameters(14, 15, Color.RED, "waterway"))
-//            // accomodation
-            Map.entry("house", new WayParameters(15, 5, Color.DARKGREY, "building")),
-            Map.entry("apartments", new WayParameters(15, 5, Color.DARKGREY, "building")),
-            Map.entry("barracks", new WayParameters(15, 5, Color.DARKGREY, "building")),
-            Map.entry("bungalow", new WayParameters(15, 5, Color.DARKGREY, "building")),
-            Map.entry("cabin", new WayParameters(15, 5, Color.DARKGREY, "building")),
-            Map.entry("detached", new WayParameters(15, 5, Color.DARKGREY, "building")),
-            Map.entry("dormitory", new WayParameters(15, 5, Color.DARKGREY, "building")),
-            Map.entry("farm", new WayParameters(15, 5, Color.DARKGREY, "building")),
-            Map.entry("hotel", new WayParameters(15, 5, Color.DARKGREY, "building"))
-//            // commercial
-////            Map.entry("commercial", new WayParameters(15, 3, Color.BURLYWOOD, "building")),
-//            Map.entry("industrial", new WayParameters(15, 3, Color.DARKGREY, "building")),
-//            Map.entry("kiosk", new WayParameters(15, 3, Color.DARKGREY, "building")),
-//            Map.entry("office", new WayParameters(14, 3, Color.DARKGREY, "building")),
-//            Map.entry("retail", new WayParameters(14, 3, Color.DARKGREY, "building")),
-//            Map.entry("supermarket", new WayParameters(13, 3, Color.DARKGREY, "building")),
-//            Map.entry("warehouse", new WayParameters(13, 3, Color.DARKGREY, "building"))
-    );
 
     /**
      * CreateGraph method creates road graph based on nodes and ways read by OsmReader
@@ -75,12 +32,18 @@ public class osmParser {
      * @param path path to OSM data .pbf file
      * @return road graph
      */
-    public static GraphOSM CreateGraph(String path) {
+    @SuppressWarnings({"UnstableApiUsage"})
+    public static GraphOSM CreateGraph(String path, String parametersPath) {
         FileInputStream inputStream = null;
+        Map<String, WayParameters> roadZooms = null;
         try {
             System.out.println(path);
             inputStream = new FileInputStream(path);
-        } catch (FileNotFoundException e) {
+            String parametersJson = new String(Files.readAllBytes(Paths.get(parametersPath)));
+            Type mapType = new TypeToken<Map<String, WayParameters>>() {
+            }.getType();
+            roadZooms = new Gson().fromJson(parametersJson, mapType);
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             System.exit(1);
         }
