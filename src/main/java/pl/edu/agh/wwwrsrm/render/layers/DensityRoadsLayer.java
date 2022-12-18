47e9d28b5c18edbdb54f6b1e0e23e4c38fe207f2
Tomasz Jatkowski <tomaszjatkowski42@gmail.com>
2022-12-18 15:36:38 +0100

    Move density coloring to seperate class

---
+++ src/main/java/pl/edu/agh/wwwrsrm/render/layers/DensityRoadsLayer.java
@@ -0,0 +1,107 @@
+package pl.edu.agh.wwwrsrm.render.layers;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import pl.edu.agh.wwwrsrm.graph.EdgeOSM;
+import pl.edu.agh.wwwrsrm.graph.GraphOSM;
+import pl.edu.agh.wwwrsrm.graph.NodeOSM;
+import pl.edu.agh.wwwrsrm.graph.WayOSM;
+import pl.edu.agh.wwwrsrm.model.Road;
+import pl.edu.agh.wwwrsrm.osm.WayParameters;
+import pl.edu.agh.wwwrsrm.utils.TrafficDensity;
+import pl.edu.agh.wwwrsrm.utils.coordinates.WindowXYCoordinate;
+import pl.edu.agh.wwwrsrm.utils.window.MapWindow;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DensityRoadsLayer extends DrawerLayer {
+
+    private final GraphOSM osm_graph;
+    private final MapWindow mapWindow;
+    private final Map<Integer, Double> resolution = new HashMap<Integer, Double>();
+    private final TrafficDensity trafficDensity;
+
+    public DensityRoadsLayer(double width, double height, GraphOSM osm_graph, MapWindow mapWindow, TrafficDensity trafficDensity) {
+        super(width, height);
+        this.mapWindow = mapWindow;
+        this.osm_graph = osm_graph;
+        this.trafficDensity = trafficDensity;
+        for(int i = MapWindow.MIN_ZOOM_LEVEL; i <= MapWindow.MAX_ZOOM_LEVEL; i++) {
+            double r = mapWindow.groundResolution((osm_graph.getBottomBound() + osm_graph.getTopBound())/2.0, i);
+            resolution.put(i, r);
+        }
+    }
+
+    @Override
+    public void draw(GraphicsContext gc, double delta) {
+        synchronized (trafficDensity) {
+            osm_graph.getWays().values().stream().sorted((w1, w2) -> {
+                Road r1 = trafficDensity.getRoad(w1.getWayId());
+                if(r1 == null)
+                    return -1;
+                Road r2 = trafficDensity.getRoad(w2.getWayId());
+                if(r2 == null)
+                    return 1;
+
+                int d1 = r1.getDensity();
+                int d2 = r2.getDensity();
+                return Integer.compare(d1, d2);
+            }).forEach(way -> {
+                WayParameters wayParameters = way.getEdgeParameter();
+                int wayMinZoomLevel = wayParameters.getZoomLevel();
+
+                double wayWidth = (double) wayParameters.getWayWidth() / resolution.get(mapWindow.getZoomLevel());
+                Color wayColor = wayParameters.getColor();
+
+                if (wayWidth < 1.0)
+                    return;
+
+                int density = 0;
+                Road road = trafficDensity.getRoad(way.getWayId());
+                if(road != null)
+                    density = road.getDensity();
+                if (!way.isClosed() && !wayParameters.getType().equals("waterway")) {
+                    drawWay(way, gc, delta, wayWidth, new Color(1.0, Math.max(0.0, 1.0-0.2*density), Math.max(0.0, 1.0-0.2*density), 1.0));
+
+                }
+            });
+        }
+
+
+    }
+
+    void drawWay(WayOSM way, GraphicsContext gc, double delta, double width, Color color) {
+        List<Double> xs = new ArrayList<>();
+        List<Double> ys = new ArrayList<>();
+        int size = 0;
+        boolean first = true;
+
+        for(EdgeOSM edge : way.getEdges()) {
+            NodeOSM startNode = edge.getStartNode();
+            NodeOSM endNode = edge.getEndNode();
+            if (mapWindow.isInsideWindow(startNode.getCoordinate()) || mapWindow.isInsideWindow(endNode.getCoordinate())) {
+                long id1 = java.lang.Math.min(startNode.getId(), endNode.getId());
+                long id2 = java.lang.Math.max(startNode.getId(), endNode.getId());
+
+                WindowXYCoordinate startNodeWindowXY = startNode.getCoordinate().convertToWindowXY(mapWindow);
+                WindowXYCoordinate endNodeWindowXY = endNode.getCoordinate().convertToWindowXY(mapWindow);
+                // drawing in canvas
+                if(first) {
+                    first = false;
+                    xs.add((double)startNodeWindowXY.getX());
+                    ys.add((double)startNodeWindowXY.getY());
+                    size++;
+                }
+                xs.add((double)endNodeWindowXY.getX());
+                ys.add((double)endNodeWindowXY.getY());
+                size++;
+            }
+
+        }
+        drawPath(gc, xs, ys, size, width, color);
+
+    }
+}
--- src/main/java/pl/edu/agh/wwwrsrm/utils/window/MapView.java
+++ src/main/java/pl/edu/agh/wwwrsrm/utils/window/MapView.java
@@ -4,10 +4,7 @@ package pl.edu.agh.wwwrsrm.utils.window;
 import lombok.Getter;
 import pl.edu.agh.wwwrsrm.graph.GraphOSM;
 import pl.edu.agh.wwwrsrm.render.Layer;
-import pl.edu.agh.wwwrsrm.render.layers.CarsLayer;
-import pl.edu.agh.wwwrsrm.render.layers.DebugLayer;
-import pl.edu.agh.wwwrsrm.render.layers.DecorationsLayer;
-import pl.edu.agh.wwwrsrm.render.layers.RoadsLayer;
+import pl.edu.agh.wwwrsrm.render.layers.*;
 import pl.edu.agh.wwwrsrm.utils.CarsManager;
 import pl.edu.agh.wwwrsrm.utils.TrafficDensity;
 
@@ -17,16 +14,26 @@ import java.util.List;
 public class MapView {
 
     private final MapWindow mapWindow;
-    private final List<Layer> layers;
+    private final List<Layer> closeZoomLayers;
+    private final List<Layer> farZoomLayers;
 
     public MapView(CarsManager carsManager, TrafficDensity trafficDensity, GraphOSM graphOSM, int width, int height) {
         this.mapWindow = new MapWindow(graphOSM.getTopLeftBound(), graphOSM.getBottomRightBound(), width, height);
-        this.layers = List.of(
-                new DecorationsLayer(width, height, graphOSM, mapWindow),
-                new RoadsLayer(width, height, graphOSM, mapWindow, trafficDensity),
-                new CarsLayer(width, height, graphOSM, mapWindow, carsManager),
-                new DebugLayer(width, height)
-        );
+
+        DecorationsLayer decorationsLayer = new DecorationsLayer(width, height, graphOSM, mapWindow);
+        RoadsLayer roadsLayer = new RoadsLayer(width, height, graphOSM, mapWindow);
+        DensityRoadsLayer densityRoadsLayer = new DensityRoadsLayer(width, height, graphOSM, mapWindow, trafficDensity);
+        CarsLayer carsLayer = new CarsLayer(width, height, graphOSM, mapWindow, carsManager);
+        DebugLayer debugLayer = new DebugLayer(width, height);
+
+        this.closeZoomLayers = List.of(decorationsLayer, roadsLayer, carsLayer, debugLayer);
+        this.farZoomLayers = List.of(decorationsLayer, densityRoadsLayer, debugLayer);
+    }
+
+    public List<Layer> getLayers() {
+        if(mapWindow.getZoomLevel() > 15)
+            return closeZoomLayers;
+        return farZoomLayers;
     }
 
 }
